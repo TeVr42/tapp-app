eb2e3dd0d917e67b9a034e9e4c2ad9fd125fdb30
VT42 <>
2021-04-22 17:19:48 +0000

    Rename files and document height/width

--- script-g1.js
+++ script-g1.js
@@ -1,11 +1,16 @@
-var zdrojeObrazku = ["k1.png", "k2.png","k3.png", "k4.png", "k5.png", "k6.png","k7.png", "k8.png", "k9.png", "k10.png"]
-var vsechnyKruhy = [];
+var srcImages = ["k1.png", "k2.png","k3.png", "k4.png", "k5.png", "k6.png","k7.png", "k8.png", "k9.png", "k10.png"]
 
-var documentSirka = document.documentElement.clientWidth;
-var dokumntVyska = document.documentElement.clientHeight;
+var documentDelka = document.documentElement.clientWidth;
+var documentVyska = document.documentElement.clientHeight;
+var malaObrazovka = window.matchMedia("(max-width: 600px)");
 
-var delkaX = documentSirka * 0.11;
-var delkaY = dokumntVyska * 0.11;
+if (malaObrazovka.matches) {
+var delkaX = documentDelka * 0.10;
+var delkaY = documentVyska * 0.1;
+} else {
+var delkaX = documentDelka * 0.05;
+var delkaY = documentVyska * 0.15;
+}
 var kruhX;
 var kruhY;
 
@@ -15,10 +20,10 @@ var i = 0;
 
 var skoreNapis = document.getElementById("skore");
 var tlacitkoZnovu = document.getElementById("tlacitkoZnovu");
-var tlacitko1 = document.getElementById("tlacitko1");
-var tlacitko2 = document.getElementById("tlacitko2");
-var tlacitko3 = document.getElementById("tlacitko3");
-var tlacitko4 = document.getElementById("tlacitko4");
+var tlacitko1 = document.getElementById("button1");
+var tlacitko2 = document.getElementById("button2");
+var tlacitko3 = document.getElementById("button3");
+var tlacitko4 = document.getElementById("button4");
 var napis = document.getElementById("napis-postreh");
 var tlacitkoDomu = document.getElementById("domu-postreh");
 var anchorDomu = document.getElementById("anchor-postreh");
@@ -36,43 +41,43 @@ function VytvorKruh(event) {
     if (hraBezi) {
     var kruh = document.createElement("img");
 
-    var index = Math.floor(Math.random() * zdrojeObrazku.length);
-    var zdroj = zdrojeObrazku[index];
+    var index = Math.floor(Math.random() * srcImages.length);
+    var source = srcImages[index];
 
-    kruh.setAttribute("src", "images/" + zdroj);
+    kruh.setAttribute("src", "images/" + source);
     kruh.setAttribute("id", "Kruh" + i);
     kruh.setAttribute("class", "kruh");
     kruh.setAttribute("onclick", "PriKliknuti(event)");
     document.body.appendChild(kruh);
 
-    kruhX = Math.floor(Math.random() *(documentSirka - delkaX));
-    kruhY = Math.floor(Math.random() * (dokumntVyska - delkaY));
+    kruhX = Math.floor(Math.random() *(documentDelka - delkaX));
+    kruhY = Math.floor(Math.random() * (documentVyska - delkaY));
 
     kruh.style.left = kruhX + "px";
     kruh.style.top = kruhY + "px";
 
-    vsechnyKruhy.push(kruh);
     i++;
 }
 }
 
 function PriKliknuti(event) {
     document.getElementById(event.target.id).remove();
-    vsechnyKruhy.pop();
+
     skore ++;
     if (skore < 10) {
     skoreNapis.textContent = "0" + skore;
     } else {
-    skoreNapis.textContent = skore;    
+    skoreNapis.textContent = skore;
     }
-    if (vsechnyKruhy.length == 0) {
+    var kruhy = document.getElementsByClassName("kruh");
+    if (kruhy.length == 0) {
     napis.style.visibility = "visible";
     napis.style.marginTop = "50px";
     napis.textContent = "Uklizeno!";
 
     hraBezi = false;
 
-    setTimeout(function(){  
+    setTimeout(function(){
     tlacitkoZnovu.style.visibility = "visible";
     tlacitkoDomu.style.visibility = "visible";
     anchorDomu.style.visibility = "visible";
